1a29ec1337a1a875dad108acf8a795c215f8e0c0
clarkeOSG <104869980+clarkeOSG@users.noreply.github.com>
2023-06-14 16:48:57 -0400

    add first content-block

---
+++ packages/content-blocks/custom-block-one/src/index.ts
@@ -0,0 +1,3 @@
+import CustomBlockOne from './component';
+
+export { CustomBlockOne };
---
+++ packages/content-blocks/custom-block-one/vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'custom-block-one',
+      fileName: 'custom-block-one',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      rollupTypes: true,
+      clearPureImport: true,
+    }),
+  ],
+});
