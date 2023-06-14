bae92e0c02eda34af13edb9a84d3e20bd957304b
aubeOSG <caube@osg.ca>
2023-06-14 14:45:27 -0400

    WIP - reactifying editorjs

--- packages/content-block-editor-react/src/component.tsx
+++ packages/content-block-editor-react/src/component.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useRef, useEffect, useCallback } from 'react';
+import {
+  BlockEditorConfig,
+  BlockEditorProps,
+  BlockEditorClass,
+} from './component.types';
+import BlockEditorFactory from './block-editor';
 
-const TestComponent = () => {
-  return <div>Hello World</div>;
+const BlockEditor = ({
+  holder,
+  defaultValue,
+  value,
+  onInit,
+  children,
+  ...props
+}: BlockEditorProps) => {
+  const factory = useCallback((config: BlockEditorConfig) => {
+    return new BlockEditorFactory(config);
+  }, []);
+  const holderRef = useRef(
+    holder || `content-block-editor-${Date.now().toString(16)}`
+  );
+  const editorJS = useRef<BlockEditorClass | null>(null);
+
+  useEffect(() => {
+    editorJS.current = factory({
+      holder: holderRef.current,
+    });
+
+    if (onInit) {
+      onInit(editorJS.current);
+    }
+
+    return () => {
+      editorJS.current?.destroy;
+    };
+  }, []);
+
+  return children || <div id={holderRef.current} />;
 };
 
-export { TestComponent };
+export { BlockEditor };
 
-export default TestComponent;
+export default BlockEditor;
---
+++ packages/content-block-editor-react/src/component.types.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+import { OutputData, EditorConfig } from '@scrowl/content-block-editor';
+
+export type BlockEditorOutputData = OutputData;
+export type BlockEditorConfig = EditorConfig;
+
+export interface BlockEditorClass {
+  destroy: () => Promise<void>;
+  clear: () => Promise<void>;
+  save: () => Promise<OutputData>;
+  render: (data: OutputData) => Promise<void>;
+  getInstance: () => any | null;
+};
+
+export type BlockEditorFactory = (config: EditorConfig) => BlockEditorClass;
+
+export interface BlockEditorCommons extends Omit<EditorConfig, 'data'> {
+  factory: BlockEditorFactory;
+  holder?: string;
+  value?: EditorConfig['data'];
+  defaultVlaue?: EditorConfig['data'];
+  onInit?: (editor: BlockEditorClass) => void;
+};
+
+export type BlockEditorProps = Omit<BlockEditorCommons, 'factory'> & React.AllHTMLAttributes<HTMLDivElement>;
