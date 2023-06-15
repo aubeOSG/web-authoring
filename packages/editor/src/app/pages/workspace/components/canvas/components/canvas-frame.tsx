374e540e7003cc3009be63a3c3d33bbdb30c2f33
aubeOSG <caube@osg.ca>
2023-06-15 11:34:03 -0400

    update - typing, added change/save handler

--- packages/editor/src/app/pages/workspace/components/canvas/components/canvas-frame.tsx
+++ packages/editor/src/app/pages/workspace/components/canvas/components/canvas-frame.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { BlockEditor } from '@scrowl/content-block-editor-react';
+import type {
+  BlockEditorAPI,
+  BlockEditorMutationEvent,
+} from '@scrowl/content-block-editor-react';
 import * as css from '../_canvas.scss';
 import { Error } from '../../../../../components';
 
 export const CanvasFrame = () => {
+  const onChange = useCallback(
+    (
+      api: BlockEditorAPI,
+      ev: BlockEditorMutationEvent | BlockEditorMutationEvent[]
+    ) => {
+      api.saver.save().then((data) => {
+        console.log('save data', data);
+      });
+    },
+    []
+  );
+
   return (
     <div className={css.canvasFrame}>
       <Error>
-        <BlockEditor />
+        <BlockEditor onChange={onChange} />
       </Error>
     </div>
   );
