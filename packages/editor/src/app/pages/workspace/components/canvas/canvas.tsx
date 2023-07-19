aa4e28e43e9539b85815cf89e312ca52e59d2c32
clarkeOSG <104869980+clarkeOSG@users.noreply.github.com>
2023-07-19 13:32:52 -0400

    add basic UI for collapse button

--- packages/editor/src/app/pages/workspace/components/canvas/canvas.tsx
+++ packages/editor/src/app/pages/workspace/components/canvas/canvas.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import * as css from './_canvas.scss';
 import { CanvasFrame, CanvasBreadcrumb } from './components';
+import { ui } from '@scrowl/ui';
+
+export const CollapsePaneButton = () => {
+  const handleClick = () => {
+    console.log('click');
+  };
+  return (
+    <ui.Button
+      className={css.canvasCollapseButton}
+      variant="primary"
+      onClick={handleClick}
+    >
+      <span className="material-symbols-rounded">login</span>
+      Show Pane
+    </ui.Button>
+  );
+};
 
 export const Canvas = () => {
   return (
     <div className={css.canvas}>
+      <CollapsePaneButton />
       <CanvasFrame />
       <CanvasBreadcrumb />
     </div>
